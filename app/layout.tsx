0f247c94c3b62270743c11aa6d6168bd58323562
Diego Medina <diego_eze_medina@hotmail.com>
2024-04-26 18:41:42 -0300

    Add metatags for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,46 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const APP_NAME = "Dime food";
+const APP_DEFAULT_TITLE = "Dime";
+const APP_TITLE_TEMPLATE = "Dime";
+const APP_DESCRIPTION = "Organiza tus comidas";
 
 export const metadata: Metadata = {
-  title: "Food",
-  description: "Organiza tus comidas",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
